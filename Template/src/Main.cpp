df0faa15e6a3c47ced1f4545551a8c938032d49d
D00dGuy07 <kieranaustinbrown@gmail.com>
2022-01-29 21:58:00 -0800

    Clean up main file

--- Template/src/Main.cpp
+++ Template/src/Main.cpp
@@ -1,162 +1,27 @@
 #include <iostream>
 
-#include <GLFW/glfw3.h>
-#include <glad/glad.h>
-
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include "Util/GLFWLoad.h"
 
 // Demos
 #include "Demos/PostProcessQuad.h"
 #include "Demos/ComputeTexture.h"
 
-// Enable Nvidia
+// Enable Nvidia high performace graphics
 #include <windows.h>
 extern "C" {
 	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 }
 
-#ifdef _DEBUG
-void openglErrorCallback(GLenum source, GLenum type, GLuint id,
-    GLenum severity, GLsizei length,
-    const GLchar* msg, const void* data)
-{
-    std::string _source;
-    std::string _type;
-    std::string _severity;
-
-    switch (source) {
-    case GL_DEBUG_SOURCE_API:
-        _source = "API";
-        break;
-
-    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-        _source = "WINDOW SYSTEM";
-        break;
-
-    case GL_DEBUG_SOURCE_SHADER_COMPILER:
-        _source = "SHADER COMPILER";
-        break;
-
-    case GL_DEBUG_SOURCE_THIRD_PARTY:
-        _source = "THIRD PARTY";
-        break;
-
-    case GL_DEBUG_SOURCE_APPLICATION:
-        _source = "APPLICATION";
-        break;
-
-    case GL_DEBUG_SOURCE_OTHER:
-        _source = "UNKNOWN";
-        break;
-
-    default:
-        _source = "UNKNOWN";
-        break;
-    }
-
-    switch (type) {
-    case GL_DEBUG_TYPE_ERROR:
-        _type = "ERROR";
-        break;
-
-    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-        _type = "DEPRECATED BEHAVIOR";
-        break;
-
-    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-        _type = "UNDEFINED BEHAVIOR";
-        break;
-
-    case GL_DEBUG_TYPE_PORTABILITY:
-        _type = "PORTABILITY";
-        break;
-
-    case GL_DEBUG_TYPE_PERFORMANCE:
-        _type = "PERFORMANCE";
-        break;
-
-    case GL_DEBUG_TYPE_OTHER:
-        _type = "OTHER";
-        break;
-
-    case GL_DEBUG_TYPE_MARKER:
-        _type = "MARKER";
-        break;
-
-    default:
-        _type = "UNKNOWN";
-        break;
-    }
-
-    switch (severity) {
-    case GL_DEBUG_SEVERITY_HIGH:
-        _severity = "HIGH";
-        break;
-
-    case GL_DEBUG_SEVERITY_MEDIUM:
-        _severity = "MEDIUM";
-        break;
-
-    case GL_DEBUG_SEVERITY_LOW:
-        _severity = "LOW";
-        break;
-
-    case GL_DEBUG_SEVERITY_NOTIFICATION:
-        _severity = "NOTIFICATION";
-        break;
-
-    default:
-        _severity = "UNKNOWN";
-        break;
-    }
-
-    printf("\n0x%X: %s of %s severity, raised from %s: %s\n\n",
-        id, _type.c_str(), _severity.c_str(), _source.c_str(), msg);
-}
-#endif // _DEBUG
-
 int main(void)
 {
-	// Make Window
-	GLFWwindow* window;
+	// Load OpenGL/GLFW
+	GLFWwindow* window = LoadGLFW();
 
-	if (!glfwInit())
+	if (window == NULL)
 		return -1;
-
-	int windowWidth = 1280;// 1280;
-	int windowHeight = 720;// 720;
-
-#ifdef _DEBUG
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
-#endif // !_DEBUG
-	window = glfwCreateWindow(windowWidth, windowHeight, "Hello World!", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-	glfwSetWindowSizeLimits(window, 100, 100, GLFW_DONT_CARE, GLFW_DONT_CARE);
-
-	// Load OpenGL functions
-	int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-	if (!status)
-		std::cout << "Failed to load Glad!" << std::endl;
-
-#ifdef _DEBUG
-    glEnable(GL_DEBUG_OUTPUT);
-    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-	glDebugMessageCallback(openglErrorCallback, nullptr);
-#endif // !_DEBUG
-
-	// Print debug info
-	std::cout << glGetString(GL_VERSION) << "\n";
-	std::cout << glGetString(GL_RENDERER) << "\n\n";
 
 	// Run application
-	Demo* demo = new ComputeTexture(window);
+	Demo* demo = new PostProcessQuad(window);
 	demo->run();
     delete demo;
 
